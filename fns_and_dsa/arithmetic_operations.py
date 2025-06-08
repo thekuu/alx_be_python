61cce18e3ba5dc4dbe9c3120906122b3647faca9
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-08 12:30:32 +0300

    Arithmetic Operations Function updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1, num2, operation):
-    if operation == '+':
+    if operation == "add":
         return num1 + num2
-    elif operation == '-':
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == '*':
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == '/':
+    elif operation == "divide":
         if num2 == 0:  # Check for division by zero
             return "Error: Division by zero"
         return num1 / num2
