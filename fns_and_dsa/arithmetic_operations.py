94549709b9fccb6fa937bd83b9badc696a462433
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-08 12:24:09 +0300

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2==0:
+                return "Error: Division by zero is not allowed."
+            else:
+                return num1 / num2 
+        case _:
+            return "Invalid operation"
