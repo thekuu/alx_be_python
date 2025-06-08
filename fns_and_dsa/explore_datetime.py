c0eeee8bb2003c033f1a725c6f1c1d4d8e98cb76
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-08 13:26:13 +0300

    Explore `datetime` Module Updated

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,7 +9,7 @@ display_current_datetime()
 
 def calculate_future_date():
     try:
-        days = int(input("Enter number of days to add (positive integer): "))
+        days = int(input("Enter the number of days to add to the current date: "))
         
         if days < 0:
             print("Please enter a positive number of days.")
