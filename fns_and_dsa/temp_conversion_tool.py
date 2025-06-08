ef7e66f6af4a909004eb04eef9af3cc443975074
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-08 13:40:32 +0300

    Temperature Conversion Tool updated

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,17 +3,22 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-Temp = float(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+try:
+    Temp = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
-if unit == "C":
-    converted = convert_to_fahrenheit(Temp)
-    print(f"{Temp}°C is {converted}°F")
-elif unit == "F":
-    converted = convert_to_celsius(Temp)
-    print(f"{Temp}°F is {converted}°C")
-else:
-    print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+    if unit == "C":
+        converted = convert_to_fahrenheit(Temp)
+        print(f"{Temp}°C is {converted}°F")
+    elif unit == "F":
+        converted = convert_to_celsius(Temp)
+        print(f"{Temp}°F is {converted}°C")
+    else:
+        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+        
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
