60fb8ce86b3dd90879570c6e9e817c2670b4742e
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-25 20:07:11 +0300

    Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
 
-    def details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -34,4 +34,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.details())
+            print(book)  # Will automatically use __str__
